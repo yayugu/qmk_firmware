002f2cf12eafeaa8e804b232f4abc03dd56fc23f
yayugu <yayugu@gmail.com>
2018-06-14 02:44:54 +0900

    fix

--- keyboards/dactyl-manuform/keymaps/5x6-default/keymap.c
+++ keyboards/dactyl-manuform/keymaps/5x6-default/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                        KC_TAB,  KC_BSLS,
                                KC_SPC,   KC_LGUI,
                                KC_ESC,   KC_LCTRL,
-                               _FN,      KC_LALT,
+                               MO(_FN),  KC_LALT,
         // right hand
                      KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS,   KC_EQL,
                      KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC,   KC_RBRC,
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       KC_LBRC, KC_RBRC,
         KC_RGUI, KC_ENTER,
         KC_BSPC, KC_RSHIFT,
-        KC_RALT, _FN),
+        KC_RALT, MO(_FN)),
 [_FN] = KEYMAP_5x6_7(
   // left hand
    _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,
