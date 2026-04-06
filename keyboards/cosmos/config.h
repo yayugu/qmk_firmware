fa53bbba0520555f946161ad4fb3b77a42ce3e0a
Yuya Yaguchi <yayugu@gmail.com>
2026-04-06 23:36:52 +0900

    cosmos

---
+++ keyboards/cosmos/config.h
@@ -0,0 +1,15 @@
+// You shouldn't need to edit any of this.
+
+#pragma once
+
+//#define EE_HANDS
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+
+#define SPLIT_USB_DETECT
+#define SERIAL_USART_FULL_DUPLEX
+#define SERIAL_USART_PIN_SWAP
+#define SERIAL_USART_TX_PIN GP0
+#define SERIAL_USART_RX_PIN GP1
+
+//#define MASTER_LEFT
