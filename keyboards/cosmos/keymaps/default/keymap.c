64819c576dcca06776f3a3417e77d5a32ae5a4cb
yayugu <yayugu@gmail.com>
2026-09-04 00:08:07 +0900

    update

--- keyboards/cosmos/keymaps/default/keymap.c
+++ keyboards/cosmos/keymaps/default/keymap.c
@@ -21,14 +21,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,    KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,
     KC_LCTL,   KC_A,    KC_S,    KC_D,   KC_F,   KC_G,
     KC_LSFT,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
-                                            KC_LGUI,  KC_SPC, KC_LALT,
-                                                      KC_ESC, MO(_FN),
+                                            KC_F20,   KC_SPC, KC_LGUI,
+                                                      KC_LALT, MO(_FN),
             // right hand
                         KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS,   KC_EQL,
                         KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC,   KC_RBRC,
                         KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,   KC_BSLS,
                         KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_GRV,    KC_RSFT,
-            KC_BSPC, KC_ENTER, KC_RGUI,
+            KC_BSPC, KC_ENTER, KC_F21,
             MO(_FN), KC_RCTL),
     [_FN] = LAYOUT(
     // left hand
